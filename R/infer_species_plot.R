e8a010eecf6d66cf784cccd11c2c24115aa9d2ae
bschilder <brian_schilder@alumni.brown.edu>
2021-10-29 10:32:50 -0400

    Try to resolve vignette building error with hrbrthemes::import_roboto_condensed()

--- R/infer_species_plot.R
+++ R/infer_species_plot.R
@@ -2,6 +2,11 @@ infer_species_plot <- function(matches,
                                show_plot = TRUE){
     percent_match <- NULL
     
+    #### Resolves the issue described here ####
+    # https://stackoverflow.com/a/66423481/13214824
+    requireNamespace("hrbrthemes")
+    hrbrthemes::import_roboto_condensed()
+    #### Plot ####
     gg <- ggplot(matches, aes(y=species, 
                               x=percent_match, 
                               fill=percent_match, 
