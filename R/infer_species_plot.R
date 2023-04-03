8b9f2f32be78bfaa49f1028eb6ada51e11fab0e6
bschilder <brian_schilder@alumni.brown.edu>
2023-04-03 10:48:28 +0100

    Remove unncessary Suggests

--- R/infer_species_plot.R
+++ R/infer_species_plot.R
@@ -10,23 +10,7 @@ infer_species_plot <- function(matches,
                                show_plot = TRUE){
     
     percent_match <- NULL
-    requireNamespace("ggplot2")
-    #### Resolves the issue described here ####
-    # https://stackoverflow.com/a/66423481/13214824
-    # Solution:
-    ## https://gi th ub.com/hrbrmstr/hrbrthemes/issues/56#issuecomment-689857655
-    if(Sys.info()["sysname"] == "Darwin"){
-        requireNamespace("knitr")
-        knitr::opts_chunk$set(dev=c('png', 'cairo_pdf'),
-                              comment="", 
-                              echo=TRUE,
-                              warning=FALSE, 
-                              cache=FALSE,
-                              message=FALSE) 
-    }
-    # if(requireNamespace("hrbrthemes")){
-    #     hrbrthemes::import_roboto_condensed()    
-    # } 
+    requireNamespace("ggplot2") 
     #### Plot ####
     gg <- ggplot2::ggplot(matches, 
                           ggplot2::aes(y=species, 
